1a96507b003f6e68692575d7a5be203de0b4a59e
aymonj <jerome@webaddict.ch>
2026-01-30 15:02:02 +0000

    Fix lakhouse name

--- 03-Machine Learning/00_Init.py
+++ 03-Machine Learning/00_Init.py
@@ -1,8 +1,8 @@
 # Databricks notebook source
 # MAGIC %sql
-# MAGIC USE CATALOG levkiwi_lakehouse;
+# MAGIC USE CATALOG jeromeaymon_lakehouse;
 # MAGIC
-# MAGIC CREATE DATABASE IF NOT EXISTS levkiwi_lakehouse.ml_sandbox;
+# MAGIC CREATE DATABASE IF NOT EXISTS jeromeaymon_lakehouse.ml_sandbox;
 
 # COMMAND ----------
 
@@ -13,7 +13,7 @@
 
 from pyspark.sql.functions import col
 
-file_path = f"/Volumes/levkiwi_lakehouse/ml_sandbox/data/train.csv"
+file_path = f"/Volumes/jeromeaymon_lakehouse/ml_sandbox/data/train.csv"
 train_df = spark.read.csv(file_path, header="true", inferSchema="true")
 train_df = train_df.withColumn("PassengerId", col("PassengerId").cast("string")) \
                    .withColumn("VIP", col("VIP").cast("int")) \
